3c9037195b51a31b4af9bcf7ca31731e923b2ef8
Tom Dolan <tgdolan@gmail.com>
2015-08-12 11:53:33 +0000

    Add integration with test.chuck
    Will be utilizing test.chuck's regex generator as the basis
    for higher level generators for typical web form input values
    like names, addresses, phone numbers, ssns, etc.
    
    --Tom

--- src/gengen/core.clj
+++ src/gengen/core.clj
@@ -1,6 +1,20 @@
-(ns gengen.core)
+(ns gengen.core
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+    (:require
+            [com.gfredericks.test.chuck :as chuck]
+            [com.gfredericks.test.chuck.generators :as gen']
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]]))
+
+(def int-vector (gen/vector (gen/fmap #(+ 1 %) gen/pos-int)))
+
+(defn get-int-vector
+  [size]
+  (gen/sample int-vector size))
+
+(defn chuck-sample
+  []
+  (gen/sample (gen'/string-from-regex #"([☃-♥]{3}|B(A|OO)M)*")))
+
