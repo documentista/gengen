796c7f4479a6e1666eb9dab94a3d5c121e0ad9e0
Tom Dolan <tgdolan@gmail.com>
2015-08-13 12:13:34 +0000

    Add functions for gen'ing zip codes / email addies
    Need to look at using vars to hold regex values.
    
    --Tom

--- src/gengen/core.clj
+++ src/gengen/core.clj
@@ -10,6 +10,12 @@
 
 (def int-vector (gen/vector (gen/fmap #(+ 1 %) gen/pos-int)))
 
+
+;; regexes from regular-expressions.info
+(def email-regex "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}$")
+
+(def postal-code-regex "[0-9]")
+
 (defn get-int-vector
   [size]
   (gen/sample int-vector size))
@@ -17,4 +23,12 @@
 (defn chuck-sample
   []
   (gen/sample (gen'/string-from-regex #"([☃-♥]{3}|B(A|OO)M)*")))
+
+(defn gen-emails
+  [sample-size]
+  (gen/sample (gen'/string-from-regex #"^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}$")))
+
+(defn gen-postal-codes
+  [sample-size]
+  (gen/sample (gen'/string-from-regex #"[0-9]{5}")))
 
